f4b129b8550c73120cac6c466438df55abd555e7
Gilles <gillesvanderseypen@gmail.com>
2022-04-07 14:30:15 +0200

    finished anecdotes exercise

--- part 1/anecdotes/src/App.js
+++ part 1/anecdotes/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
-
 import { useState } from 'react'
+
+const Title = ({text}) => <h1>{text}</h1>
+
+const Highest = ({array, anecdotes}) => {
+  const highestVote = Math.max(...array);
+  const index = array.indexOf(highestVote)
+  console.log('highest votes', highestVote)
+  console.log('index of higest vote', index)
+  console.log(anecdotes[index])
+  return (
+    <div>{anecdotes[index]} has {highestVote} votes</div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -14,10 +26,29 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0])
+
+  const generateRandom = () => {
+    let max = anecdotes.length 
+    let random = Math.floor(Math.random() * max)
+    setSelected(random)
+  }
+
+  const addVote = () => {
+    const newArray = [...array]
+    newArray[selected] = newArray[selected] + 1
+    setArray(newArray)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <Title text={'Anecdote of the Day'}/>
+      <p>{anecdotes[selected]}</p>
+      <p>has {array[selected]} votes</p>
+      <button onClick={addVote}>vote</button>
+      <button onClick={generateRandom}>next anecdote</button>
+      <Title text={'Anecdote with the most votes'}/>
+      <Highest array={array} anecdotes={anecdotes}/>
     </div>
   )
 }
